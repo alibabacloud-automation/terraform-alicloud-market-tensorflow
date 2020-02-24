71847ab6931ca8308b98392f3fea236f09364e31
zhouqilin <1373413131@qq.com>
2020-02-24 17:29:30 +0800

    improve(market-tensorflow): add more outputs.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0 (Unreleased)
 
+IMPROVEMENTS：
+
+- add more outputs [GH-2]( https://github.com/terraform-alicloud-modules/terraform-alicloud-market-tensorflow/pull/2)
+
 - **NEW:** `market-tensorflow` [GH-1]( https://github.com/terraform-alicloud-modules/terraform-alicloud-market-tensorflow/pull/1)
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -2,3 +2,18 @@ output "this_ecs_instance_public_ip" {
   description = "The ecs instance public ip."
   value       = module.market-tensorflow.this_ecs_instance_public_ip
 }
+
+output "this_instance_id_of_tensorflow_with_ecs" {
+  description = "The instance id of tensorflow."
+  value       = module.market-tensorflow.this_ecs_instance_id
+}
+
+output "this_image_id_of_tensorflow_with_ecs" {
+  description = "The image id of tensorflow."
+  value       = module.market-tensorflow.this_ecs_instance_image_id
+}
+
+output "this_vswitch_id_of_tensorflow_with_ecs" {
+  description = "The vswitch id of tensorflow."
+  value       = module.market-tensorflow.this_vswitch_id
+}
